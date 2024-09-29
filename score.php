dffd887413c3bc2c24867e2c3d434afa3ee355db
dobincho <na096rie@icloud.com>
2024-09-29 22:26:16 +0900

    kadai_005提出

---
+++ score.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta chaset="utf-8">
+  <title>テストの平均点</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    $score1 = 80;
+    $score2 = 60;
+    $score3 = 55;
+    $score4 = 40;
+    $score5 = 100;
+    $score6 = 25;
+    $score7 = 80;
+    $score8 = 95;
+    $score9 = 30;
+    $score10 = 60;
+
+    //テストの平均点
+
+    echo ($score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 +$score10)/10;
+    ?>
+    </p>
+  </body>
+  
+  </html>
